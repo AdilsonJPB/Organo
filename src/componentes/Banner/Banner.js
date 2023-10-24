e9cf20e7c0e36c13136b92fd498bbc46f203d5d8
Adilson Junior <adilsonjunior@FSLabs-Mac-mini-3.local>
2023-10-24 18:28:48 -0400

    mandando projeto

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX
+    return (
+        <header className="banner">
+        <img src="/imagens/banner.png" alt="o banner principal da página netlix"/>
+        </header>
+    )
+}
+
+export default Banner
